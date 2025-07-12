01484064936fdd4652fe8d3cf372f34c8f8f8c2a
Gonzalo Calderon <gonzacalderon13788@mail.com>
2025-07-11 21:32:05 -0300

    correccion days

--- src/utils/tiempo.js
+++ src/utils/tiempo.js
@@ -1,5 +1,8 @@
-import dayjs from '../utils/tiempo.js';
-import 'dayjs/locale/es';
+// src/utils/tiempo.js (BACKEND)
+
+import dayjs from 'dayjs';
+import 'dayjs/locale/es.js'; // 👈 esta línea es la clave
+
 dayjs.locale('es');
 
 const HORA_CORTE = 6;
@@ -17,5 +20,4 @@ export const esHoyOperativo = (fecha) => {
   return dayjs(fecha).isSame(getFechaOperativa(), 'day');
 };
 
-// al final de utils/fecha.js
 export default dayjs;
